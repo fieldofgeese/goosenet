62c474248afb70a8bc8eb5792149389571abde9d
AntonJohansson <97antjoh@gmail.com>
2022-05-28 03:58:14 +0200

    Output into build dir, and strict compile settings

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-clang -g src/common/log.c src/common/socket.c src/client.c -o gn-client
-clang -g src/common/log.c src/common/socket.c src/server.c -o gn-server
+BUILD_DIR=build
+
+COMMON_SRCS="src/common/log.c \
+             src/common/socket.c"
+
+COMMON_FLAGS="-Wextra -Wall -std=gnu11"
+DEBUG_FLAGS="-fsanitize=address -g -O0"
+
+[ ! -d ${BUILD_DIR} ] && mkdir ${BUILD_DIR}
+
+clang -o ${BUILD_DIR}/gn-client ${COMMON_FLAGS} ${DEBUG_FLAGS} ${COMMON_SRCS} src/client.c
+clang -o ${BUILD_DIR}/gn-server ${COMMON_FLAGS} ${DEBUG_FLAGS} ${COMMON_SRCS} src/server.c
